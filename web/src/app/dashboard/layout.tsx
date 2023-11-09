7c60158277c99a578315d3a1fb23a50bfae59d79
Matt Deal <matthew.deal@rocketlab.com.au>
2023-11-09 17:58:45 +1100

    adds side nav, and topnav

--- web/src/app/dashboard/layout.tsx
+++ web/src/app/dashboard/layout.tsx
@@ -3,38 +3,47 @@ import Link from 'next/link';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex h-screen flex-col">
       <nav>
         <div className="flex items-center justify-between border-b border-gray-200 bg-white px-5 py-5">
           <div className="flex items-center space-x-4">
-            <Link href="/">
-              <div className="flex-shrink-0">Some Logo</div>
+            <Link className="flex-shrink-0" href="/">
+              Some Logo
             </Link>
           </div>
-          <div className="flex flex-row gap-2">
-            <Link href="/dashboard">
-              <div className="hidden md:block">Dashboard</div>
-            </Link>
-            <Link href="/dashboard">
-              <div className="hidden md:block">Dashboard</div>
-            </Link>
-            <Link href="/dashboard">
-              <div className="hidden md:block">Dashboard</div>
+          <div className=" mx-auto flex flex-row gap-2 bg-white">
+            {/* We can group each link so that the hover effect is applied to each individually */}
+            <div className="">
+              <Link
+                className="0 hidden rounded p-2 md:block"
+                href="/dashboard1">
+                Dashboard 1
+              </Link>
+            </div>
+            <div className="">
+              <Link className="hidden rounded p-2 md:block" href="/dashboard2">
+                Dashboard 2
+              </Link>
+            </div>
+            <div className="">
+              <Link className="hidden rounded p-2 md:block" href="/dashboard3">
+                Dashboard 3
+              </Link>
+            </div>
+          </div>
+          <div className="">
+            <Link className="hidden rounded p-2 md:block" href="/profile">
+              Profile
             </Link>
           </div>
-          <Link href="/dashboard">
-            <div className="hidden md:block">Profile</div>
-          </Link>
         </div>
       </nav>
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="flex flex-1 overflow-hidden md:flex-row">
         <div className="w-full flex-none md:w-64">
           <SideNav />
         </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-          {children}
-        </div>
+        <div className="flex-grow overflow-y-auto p-6 md:p-12">{children}</div>
       </div>
-    </>
+    </div>
   );
 }
--- web/src/components/sidenav.tsx
+++ web/src/components/sidenav.tsx
@@ -1,14 +1,24 @@
+// SideNav component
+import Link from 'next/link';
+import NavLinks from './dashboard/navLinks';
+
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-200 md:block">
-          <form>
-            <button className="hover:bg-gray-00 flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium text-black hover:text-gray-600 md:flex-none md:justify-start md:p-2 md:px-3">
-              <div className="hidden md:block">Sign Out</div>
-            </button>
-          </form>
+    <div className="flex h-full flex-col overflow-y-auto px-3 py-4 md:px-2">
+      <div className="flex grow flex-col justify-between space-y-2 rounded-lg bg-gray-200 p-4">
+        {/* Flex container for links */}
+
+        <div className="space-y-2">
+          <NavLinks />
+          {/* Add more links here */}
         </div>
+
+        {/* Form with the button at the bottom */}
+        <form className="mt-auto w-full">
+          <button className="flex h-[48px] w-full items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium text-black hover:bg-gray-300 hover:text-gray-600">
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
       </div>
     </div>
   );
