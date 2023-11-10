2345b270c773ebeb0ddd3f81e684049afd8657b4
Matt Deal <matthew.deal@rocketlab.com.au>
2023-11-10 16:15:22 +1100

    adds norepg and creates a sql connet helper

---
+++ web/src/lib/data.ts
@@ -0,0 +1,40 @@
+import { formatCurrency } from '@/utils/helpers';
+import { unstable_noStore as noStore } from 'next/cache';
+import getDbClient from './sqlClient';
+
+export async function fetchCardData() {
+  const sql = await getDbClient();
+  noStore();
+  try {
+    // You can probably combine these into a single SQL query
+    // However, we are intentionally splitting them to demonstrate
+    // how to initialize multiple queries in parallel with JS.
+    const invoiceCountPromise = sql.query(`SELECT COUNT(*) FROM invoices`);
+    const customerCountPromise = sql.query(`SELECT COUNT(*) FROM customers`);
+    const invoiceStatusPromise = sql.query(`SELECT
+           SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+           SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+           FROM invoices`);
+
+    const data = await Promise.all([
+      invoiceCountPromise,
+      customerCountPromise,
+      invoiceStatusPromise,
+    ]);
+
+    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
+    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
+    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+
+    return {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    };
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to card data.');
+  }
+}
