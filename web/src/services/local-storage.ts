3947ede76508ba3fa9be747ef3f26884ba0d45a3
Davin McDonald <davin.mcdonald@rocketlab.com.au>
2023-11-21 13:23:14 +1100

    remove comments

--- web/src/services/local-storage.ts
+++ web/src/services/local-storage.ts
@@ -3,27 +3,19 @@
 import { cookies } from 'next/headers'
 
 export const cookieStoreGet = async (key: string) => {
-  // return localStorage.getItem(key)
   const cookieStore = cookies()
   const token = cookieStore.get(key)
   return token?.value === '' ? undefined : token?.value
 }
 
 export const cookieStoreSet = (key: string, value: string) => {
-  // localStorage.setItem(key, value)
   cookies().set(key, value, {
     httpOnly: true, // Secure the cookie from client-side scripts
     sameSite: 'lax', // CSRF protection
     path: '/', // Cookie will be accessible on all pages
-    // Add other options like 'secure' or 'expires' as needed
   })
 }
 
 export const cookieStoreRemove = (key: string) => {
-  // localStorage.removeItem(key)
   cookies().delete(key)
-}
-
-export const cookieStoreClear = async () => {
-  // localStorage.clear()
 }
